285e10844ebb4992d88dbe850cb797bbe88b1d03
Daniel George <dan20geo20@gmail.com>
2022-11-07 16:49:55 -0800

    fixed adding directly to shadow dom and added style to shadow dom

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -8,7 +8,7 @@ class RecipeCard extends HTMLElement {
 
     // EXPOSE - START (All expose numbers start with A)
     // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
-    const shadow = this.attachShadow({ mode: "open" });
+     this.attachShadow({ mode: "open" });
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
     const article = document.createElement("article");
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
@@ -16,7 +16,6 @@ class RecipeCard extends HTMLElement {
     
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
     style.textContent = `
-    <style>
   * {
     font-family: sans-serif;
     margin: 0;
@@ -92,11 +91,10 @@ class RecipeCard extends HTMLElement {
     color: #70757A;
     font-size: 12px;
   }
-</style> 
 `
     // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
-    shadow.appendChild(style);
-    shadow.appendChild(article);
+    this.shadowRoot.append(style, article);
+
   }
 
   /**
@@ -126,7 +124,7 @@ class RecipeCard extends HTMLElement {
 
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
     //let article = document.querySelector('article');
-    let article = this.shadowRoot;
+    let article = this.shadowRoot.childNodes[1];
 
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
